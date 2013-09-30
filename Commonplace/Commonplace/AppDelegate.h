38a1757476bd36b79d51e3f4a9aff924ab61dc0e
Peter Sauer <petercsauer@me.com>
2013-09-30 14:10:08 -0400

    Added Basic Project

---
+++ Commonplace/Commonplace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Commonplace
+//
+//  Created by Peter Sauer on 9/30/13.
+//  Copyright (c) 2013 Peter Sauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
